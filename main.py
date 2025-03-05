42000cad14f814bc84aa1972df313151752277e1
Naresh Karthigeyan <nareshkarthigeyan.2005@gmail.com>
2025-03-06 02:05:06 +0530

    done

--- main.py
+++ main.py
@@ -60,21 +60,23 @@ def parsing(input):
     return res
 
 def compression(input):
-    a = 0x6a09e667
-    b = 0xbb67ae85
-    c = 0x3c6ef372
-    d = 0xa54ff53a
-    e = 0x510e527f
-    f = 0x9b05688c
-    g = 0x1f83d9ab
-    h = 0x5be0cd19
+    a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p = [
+    0xcbbb9d5d, 0x629a292a, 0x9159015a, 0x152fecd8,
+    0x67332667, 0x8eb44a87, 0xdb0c2e0d, 0x47b5481d,
+    0xe19b48a8, 0xc5c95a63, 0xa1e38b93, 0x29d070bb,
+    0x5c26c926, 0x33a7d98e, 0x4aab5db7, 0x4f7a04dd
+]
 
-    constant = [a, b, c, d, e, f, g, h]
-    res = []
     for i in range(len(input)):
-        val = int(input[i], 2) + int(sigma(format(constant[(i + 2) % len(constant)], '032b')), 2) + int(bigM(format(constant[(i + 5) % len(constant)], '032b')), 2) + left_rotate(constant[i % len(constant)] * 9, 11)
-        res.append(format(val, '08b'))
-    return res
+        t1 = (int(input[i], 2) + int(sigma(format(e, '032b')), 2) + int(bigM(format(g, '032b')), 2) + left_rotate(a, 5, 32)) % 4294967295
+        t2 = (int(bigM(format(j, '032b')), 2) + int(sigma(format(l, '032b')), 2) + right_rotate(n, 7, 32)) % 4294967295
+        p, o, n, m = o, n, m, (l + t1) % 4294967295
+        l, k, j, i = k, j, i, (t1 + t2) % 4294967295
+        h, g, f, e = g, f, e, (d + t1) % 4294967295
+        d, c, b, a = c, b, a, (t1 + t2) % 4294967295
+
+    return [format(x, '08b') for x in (a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p)]
+
 
 def toHash(input):
     hash = ''
@@ -93,6 +95,7 @@ def nar512(input):
     # print(toHash(parsed))
     compressed = compression(parsed)
     print(toHash(compressed))
+    print(len(toHash(compressed)))
 
 
-nar512("abcd")
+nar512("NARESH KARTHIGEYAN")
